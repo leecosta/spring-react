05489279a4e51010de26d5a6eb0248efc9293620
Letícia Costa Mancini <leticia.mancini@ibm.com>
2022-07-11 22:49:51 -0300

    Project clean

---
+++ dsmeta/backend/src/main/java/com/springreact/leecosta/LeecostaApplication.java
@@ -0,0 +1,13 @@
+package com.springreact.leecosta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeecostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeecostaApplication.class, args);
+	}
+
+}
